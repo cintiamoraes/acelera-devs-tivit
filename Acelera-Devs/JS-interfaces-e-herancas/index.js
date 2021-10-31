0fb910cb5098a6775aaf44bfde361fc19a6b3861
Cintia <cintiamoraes76@gmail.com>
2021-10-31 15:21:41 -0300

    Interfaces e heranças

--- Acelera-Devs/JS-interfaces-e-herancas/index.js
+++ Acelera-Devs/JS-interfaces-e-herancas/index.js
@@ -2,22 +2,18 @@
 //aqui estou criando uma instância do meu molde (criando os meus objetos a partir do molde), ou seja, da minha classe
 
 import{Cliente} from "./Cliente.js"
-import {ContaCorrente} from "./ContaCorrente.js"
-import { Conta } from "./Conta.js";
-import { ContaPoupanca } from "./ContaPoupanca.js";
+import {ContaCorrente} from "./Conta/ContaCorrente"
+import { ContaPoupanca } from "./Conta/ContaPoupanca";
+import { ContaSalario } from "./Conta/ContaSalario";
 
 const cliente1 = new Cliente("Ricardo",1234567988);
 
 
 //os meus clientes são meus objetos
+const contaCorrenteRicardo = new ContaCorrente( cliente1, 1001);
+const contaPoupanca = new ContaPoupanca(50, cliente1, 1001);
+const contaSalario = new ContaSalario(cliente1);
+contaSalario.depositar(100);
+contaSalario.sacar(10);
 
-const contaCorrenteRicardo = new ContaCorrente(0,cliente1, 1001);
-contaCorrenteRicardo.depositar=500;
-contaCorrenteRicardo.sacar(100);
-
-const ContaPoupanca = new ContaPoupanca(50, cliente1, 1001);
-ContaPoupanca.sacar(10);
-
-const conta = new Conta(0, cliente1, 1001);
-
-console.log(conta);
+console.log(contaSalario);
